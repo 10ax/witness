1c51661e52af7b30d902b206ef934fb54b7dfc98
Katiacrisci <katcrisci@gmail.com>
2024-01-28 22:05:20 +0100

    Added blurred background, hopefully fixed layout on mobile

--- script.js
+++ script.js
@@ -12,8 +12,7 @@ nob.addEventListener("mousemove", moveButton);
 nob.addEventListener("click", moveButton);
 
 function random(min, max) {
-  let numero = Math.floor(Math.random() * (max - min + 1) + min)
-  return numero
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 let sib = document.getElementById("Si")
@@ -21,7 +20,8 @@ let sib = document.getElementById("Si")
 sib.addEventListener("click", () => {
   document.getElementById("p").removeAttribute("hidden")
   console.log("click")
-  document.getElementById("1").style.color = "transparent";
-  document.getElementById("Si").style.display = "none";
-  document.getElementById("No").style.display = "none";
+  document.getElementById("1").remove();
+  document.getElementById("Si").remove();
+  document.getElementById("No").remove();
+  document.querySelector("section").remove()
 })
